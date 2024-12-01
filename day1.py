d604b4965063cb07ff32db86d351921b572bfe14
Sam Bahrami <bahrami.sam@gmail.com>
2024-12-01 23:21:32 +1030

    formatted

--- day1.py
+++ day1.py
@@ -1,5 +1,6 @@
 input_file = "inputs/day1.txt"
 
+
 def read_list(input):
     lines = input.split("\n")
     lists = {}
@@ -13,6 +14,7 @@ def read_list(input):
             lists[i].append(int(element))
     return lists
 
+
 def compare_list_pair_distances(lista, listb):
     """Compare the distance between each element of lista and listb"""
     distances = []
@@ -20,14 +22,6 @@ def compare_list_pair_distances(lista, listb):
         distances.append(abs(lista[i] - listb[i]))
     return distances
 
-# Read the input file as a string
-with open(input_file, "r") as file:
-    day1_input = file.read()
-
-lists = read_list(day1_input)
-lista, listb = sorted(lists[0]), sorted(lists[1])
-distances = compare_list_pair_distances(lista, listb)
-print(sum(distances))
 
 # Create a hashmap of the second list
 def count_element_frequency_in_list(number_list):
@@ -38,6 +32,15 @@ def count_element_frequency_in_list(number_list):
         frequency[number] += 1
     return frequency
 
+
+# Read the input file as a string
+with open(input_file, "r") as file:
+    day1_input = file.read()
+
+lists = read_list(day1_input)
+lista, listb = sorted(lists[0]), sorted(lists[1])
+distances = compare_list_pair_distances(lista, listb)
+print(sum(distances))
 listb_frequency = count_element_frequency_in_list(listb)
 similarity_score = 0
 for number in lista:
