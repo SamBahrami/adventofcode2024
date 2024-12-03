692f5288999e55aa9730688dd3fbaeb54a6e9352
Sam Bahrami <bahrami.sam@gmail.com>
2024-12-03 20:26:02 +1030

    day3 complete

--- day3.py
+++ day3.py
@@ -7,15 +7,22 @@ input_string = utils.read_file_as_string("inputs/day3.txt")
 def get_parts_matching_regex(regex, text):
     return re.findall(regex, text)
 
-parts = get_parts_matching_regex(r"mul\([0-9]+,[0-9]+\)", input_string)
+parts = get_parts_matching_regex(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", input_string)
 total_mult = 0
+do = True
 for part in parts:
-    # replace characters in a string 
-    part = part.replace("mul(", "")
-    part = part.replace(")", "")
-    numbers = part.split(",")
-    numbers = [int(number) for number in numbers]
-    total_mult += numbers[0] * numbers[1]
+    if part == "do()":
+        do = True
+        continue
+    elif part == "don't()":
+        do = False
+        continue
+    elif do:
+        part = part.replace("mul(", "")
+        part = part.replace(")", "")
+        numbers = part.split(",")
+        numbers = [int(number) for number in numbers]
+        total_mult += numbers[0] * numbers[1]
 
 print(total_mult)
 pass
